8171b9deb26e92d9505f48388e69be291e3ed782
mee1969 <69719628+mee1969@users.noreply.github.com>
2020-08-16 16:17:02 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,44 @@
+let projectile: Sprite = null
+scene.setBackgroundColor(9)
+let mySprite = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . b b b b . . . . . . 
+    . . . . b b 3 3 3 3 b b . . . . 
+    . . . c b 3 3 3 3 1 1 b c . . . 
+    . . c b 3 3 3 3 3 1 1 1 b c . . 
+    . c b 1 1 1 3 3 3 3 1 1 3 c c . 
+    c b d 1 1 1 3 3 3 3 3 3 3 b b c 
+    c b b d 1 3 3 3 3 3 1 1 1 b b c 
+    c b b b 3 3 1 1 3 3 1 1 d d b c 
+    . c b b b d d 1 1 3 b d d d c . 
+    . . c c b b d d b b b b c c . . 
+    . . . . c c c c c c c c . . . . 
+    . . . . . b b d 1 1 b . . . . . 
+    . . . . . b d d 1 1 b . . . . . 
+    `, SpriteKind.Player)
+game.onUpdateInterval(1000, function () {
+    projectile = sprites.createProjectileFromSprite(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . 4 4 4 4 . . . . . . 
+        . . . . 4 4 4 5 5 4 4 4 . . . . 
+        . . . 3 3 3 3 4 4 4 4 4 4 . . . 
+        . . 4 3 3 3 3 2 2 2 1 1 4 4 . . 
+        . . 3 3 3 3 3 2 2 2 1 1 5 4 . . 
+        . 4 3 3 3 3 2 2 2 2 2 5 5 4 4 . 
+        . 4 3 3 3 2 2 2 4 4 4 4 5 4 4 . 
+        . 4 4 3 3 2 2 4 4 4 4 4 4 4 4 . 
+        . 4 2 3 3 2 2 4 4 4 4 4 4 4 4 . 
+        . . 4 2 3 3 2 4 4 4 4 4 2 4 . . 
+        . . 4 2 2 3 2 2 4 4 4 2 4 4 . . 
+        . . . 4 2 2 2 2 2 2 2 2 4 . . . 
+        . . . . 4 4 2 2 2 2 4 4 . . . . 
+        . . . . . . 4 4 4 4 . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `, mySprite, randint(-25, 25), randint(-25, 25))
+    projectile.lifespan = 3000
+    if (projectile.vx < 0) {
+        mySprite.image.flipX()
+    }
+})
